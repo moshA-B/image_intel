94fa079f1e983527fe81d10f5045c7d5813eb6ae
elihalperin2003 <eelihalperin@gmail.com>
2026-03-12 15:38:53 +0200

    הוספת מספרים לציר הזמן, שינוי עמוד הפתיחה

--- src/timeline.py
+++ src/timeline.py
@@ -8,7 +8,7 @@ def create_timeline(images_data):
 
     html = '<div style="position:relative; padding:20px;">'
     html += '<div style="position:absolute; left:50%; width:2px; height:95%; background:#333;"></div>'
-    pin_color = ['blue', 'orange', 'green', 'purple', 'pink', 'brown', 'cadetblue',
+    pin_color = ['pink','blue', 'orange', 'green', 'purple',  'brown', 'cadetblue',
                  'black', 'red']
     color_dict = {}
     for img in dated_images:
@@ -24,14 +24,14 @@ def create_timeline(images_data):
     for i, img in enumerate(dated_images):
         date_only = img["datetime"].split()[0]
         text_color = color_dict[date_only]
-        sides = {0: "right", 1 : "left"}
+        sides = {0: "right", 1: "left"}
+        flex_dirs = {0: "row", 1: "row-reverse"}
         if not date_only == date_for_check: num += 1
         html += f'''
         <div style="margin:20px 0; text-align:{sides[num % 2]}; color: {text_color}; position: relative;">
             <strong>{img["datetime"]}</strong><br>
             {img["filename"]}
-            <div style="position:absolute; top:50%; {sides[(num+1) % 2]}: 50%; width:10%; height:2px; background:#333;"></div>
-            <br>
+        <div style="position:absolute; top:50%; {sides[(num + 1) % 2]}: 50%; display:flex; flex-direction:{flex_dirs[num % 2]}; align-items:center; gap:8px;"><span style="font-family:sans-serif;">{i+1}</span><div style="width:50px; height:2px; background:#333;"></div></div>        <br>
             <small>{img.get("camera_model", "Unknown")}</small>
         </div>'''
         date_for_check = date_only
